5533465070adf448e32f163be1363cf0b658ce04
GregZ_ <gregz_@gregz.me>
2020-04-10 18:43:18 +0100

    Basic plugin setup
    * Added a basic pom bile that will populate the plygin.yml automatically.
    * Added basic main class and plugin.yml files.

---
+++ src/main/java/me/gregz/ssago/easter/SSAGOEaster.java
@@ -0,0 +1,42 @@
+/*
+ * The Spigot plugin for the SSAGO(https://ssago.org) Easter event within Minecraft.
+ * Copyright (C) 2020  Gregory HS (GregZ_)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.gregz.ssago.easter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The main class of the SSAGO Easter plugin, this holds the plugin
+ * initialization code and servers as the main storage for objects related to
+ * the plugin.
+ *
+ * @author GregZ_
+ * @version 1
+ * @since 1.0
+ */
+public class SSAGOEaster extends JavaPlugin {
+
+    /**
+     * Called when the plugin is enabled by the server, instantiate and store
+     * all needed objects.
+     */
+    @Override
+    public void onEnable() {
+        //TODO
+    }
+}
